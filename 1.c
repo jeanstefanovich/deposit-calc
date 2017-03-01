0b1358165c15142ded7cab880403abe0c2539168
Стефанович Жан <ip614s23@mwp01.csc.local>
2017-03-01 14:17:10 +0700

    "Начало работы над lab2"

---
+++ 1.c
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+	int srok, summ, percent;
+	cout<<"Введите срок";
+	cin>>srok;
+	cout<<"Введите сумму";
+	cin>>summ;
+	if(summ<10000)cout<<"Сумма ниже порога 10000 р."
+	if(srok<30){
+	return 0;
+}
