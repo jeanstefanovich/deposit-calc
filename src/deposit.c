767832a89dc53c87aa700e544da27e8deecf94e7
Стефанович Жан <ip614s23@awp06.csc.local>
2017-05-17 15:30:50 +0700

    Fix one of errors validation

--- src/deposit.c
+++ src/deposit.c
@@ -2,9 +2,10 @@
 #include "deposit.h"
 int deposit(int srok, double  summ){
 	using namespace std;
+	if((srok<0)||(srok>365)) return -1;
+	if(summ<10000) return -1;
 	if(srok<=30) return(summ*0.9);
 	if(srok>=31&&srok<=120&&summ<=100000) return(summ*1.02);
-
 	if(srok>=31&&srok<=120&&summ>=100000) return(summ*1.03);
 	if(srok>=121&&srok<=240&&summ<=100000) return(summ*1.06);
 	if(srok>=121&&srok<=240&&summ>=100000) return(summ*1.08);
